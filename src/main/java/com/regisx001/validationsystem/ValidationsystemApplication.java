f4ee743af53450d78d7100707a127a2dc7b209c5
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-07-23 14:04:17 +0100

    Initialize project structure with Maven, Spring Boot, and configuration files

---
+++ src/main/java/com/regisx001/validationsystem/ValidationsystemApplication.java
@@ -0,0 +1,13 @@
+package com.regisx001.validationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/regisx001/validationsystem/ValidationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regisx001.validationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

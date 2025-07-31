eb4edbb016832db3b73255350d040caf579db144
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-07-31 14:37:39 +0100

    feat: Add entity scanning and JPA repository configuration to application startup

--- src/main/java/com/regisx001/validationsystem/ValidationsystemApplication.java
+++ src/main/java/com/regisx001/validationsystem/ValidationsystemApplication.java
@@ -2,10 +2,14 @@ package com.regisx001.validationsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
+@EntityScan("com.regisx001.validationsystem.domain.entities")
+@EnableJpaRepositories("com.regisx001.validationsystem.repositories")
 public class ValidationsystemApplication {
 
 	public static void main(String[] args) {
